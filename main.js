4ae8f799325d04cb9afae15ce210206ba90949f1
Jones I. Ossai <grayxjones@gmail.com>
2022-12-13 12:17:18 +0000

    css styling and set target of pressed elements

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-const $ = (el)=> document.querySelector(`${el}`)
-const log =(e)=> console.log(e)
+const $ = (elem,scope)=> (scope || document).querySelector(elem)
+const $$ = (elem,scope)=> (scope || document).querySelectorAll(elem)
+const log =(a,b,c)=> console.log(a,b,c)
+const createElement = (parent, tag, className, content) => {
+    elem = document.createElement(tag)
+    if (className) elem.classList.add(className)
+    if (content) elem.textContent= content
+    // parent.appendChild(elem)
+}
+const addClass = (selector, className, scope) => {
+    (scope || document).querySelector(selector).classList.add(className);
+}
 
-const root = $('#root')
-log(root)
+root.addEventListener('click',tar)
+keyValue =[]
+function tar(e){
+    
+    bigScreen = $('#big-screen')
+    smallScreen = $('#small-screen')
+    log(bigScreen, smallScreen)
+    
+    keyValue.push(e.target.innerText)
+bigScreen.value = e.target.innerText
+log(keyValue) 
+if (e.target.innerText === '='){
+    log(keyValue,"yes") 
+}
+
+ return e.target.innerText
+ 
+}
+
+
+
+
