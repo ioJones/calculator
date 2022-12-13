547df6ad1a19833b9bffae76c86426a7f57fce9b
Jones I. Ossai <grayxjones@gmail.com>
2022-12-13 22:41:21 +0000

    create sqrt function

--- main.js
+++ main.js
@@ -11,45 +11,66 @@ const addClass = (selector, className, scope) => {
     (scope || document).querySelector(selector).classList.add(className);
 }
 
-root.addEventListener('click',tar)
+
 big = $('#big')
 
-log(big.reset())
 smallScreenValue =""
 bigScreenValue=""
+bigScreen = $('#big-screen')
+smallScreen = $('#small-screen')
+bigScreen.value=bigScreenValue
+root.addEventListener('click',tar)
+
 function tar(e){
-    bigScreen = $('#big-screen')
-    smallScreen = $('#small-screen')
     key = e.target.classList
     if (smallScreen.value.indexOf('=')>-1){
-        log('working')
         smallScreenValue=""
         smallScreen.value = ""
+        bigScreenValue = ""
+        bigScreen.value=""
     }
     if(key.value === "numbers"){
         bigScreenValue = bigScreenValue.concat("",e.target.innerText)
         bigScreen.value = bigScreenValue
         
 
-    }else if (key.value !== "numbers"){
-        bigScreen.value = ""
-        smallScreenValue = smallScreenValue.concat("", bigScreenValue + e.target.innerText)
-        bigScreenValue=""
-        smallScreen.value = smallScreenValue
+    }else if (key.value === "operators"){
         
-        if (e.target.innerText.indexOf('=')>-1) {
+        smallScreenValue = smallScreenValue.concat("", bigScreenValue + e.target.innerText)
+        smallScreen.value = smallScreenValue
+        bigScreenValue=""
+        bigScreen.value = ""
+        if (e.target.innerText.indexOf('=')>-1){
             ans = eval(smallScreenValue.substr(0, smallScreenValue.length-1))
             bigScreen.value=ans
-            log(ans)
         }
-        return 
-    } else if((e.target.innerText === 'del')){
-    // log(keyValue,"del") 
-    return 
-}
+            
+           
+       
+    } else if (key.value === "del"){
+        log(bigScreenValue)
+        if (bigScreenValue.length > 0){
+            del = bigScreenValue.substr(0, bigScreenValue.length - 2)
+            bigScreen.value=del
+            log(del, bigScreenValue)
+        } else{
+            log('3')
+            if (smallScreenValue.length >0){
+                smallScreenValue.substr(0, smallScreenValue.length-1)
+                
+            }
+        
+        }
 
-//  
- 
+
+    } else if (e.target.innerText.indexOf('sqrt')>-1){
+        smallScreenValue = smallScreenValue.concat("", bigScreenValue + '**0.5' )
+        
+        ans = eval(smallScreenValue)
+        bigScreen.value=ans
+        smallScreen.value = e.target.innerText + ' of ' + smallScreenValue.substr(0, smallScreenValue.length-5) +  '='
+
+    }
 }
 
 
